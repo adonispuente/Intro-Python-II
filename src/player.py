f62c4928350d4f6d55fcb5bbf6c654cb104ee8ec
Adonis Puente <adonispuente@MacBook-Pro.attlocal.net>
2020-08-06 12:02:10 -0400

    mvp met

--- src/player.py
+++ src/player.py
@@ -1,2 +1,20 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from room import Room
+
+
+class Player:
+    def __init__(self, location):
+        self.location = location
+
+    def move(self, keypress):
+        newRoom = getattr(self.location, keypress)
+        if newRoom is not None:
+            self.location = newRoom
+            # print(self.location.name)
+
+        else:
+            print('Sorry, theres no room there')
+
+    def __str__(self):
+        return 'Location: {self.location}'.format(self=self)
--- src/room.py
+++ src/room.py
@@ -1,2 +1,13 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n = None
+        self.e = None
+        self.s = None
+        self.w = None
+
+    def __str__(self):
+        return "You are now in {self.name}. {self.description}".format(self=self)
